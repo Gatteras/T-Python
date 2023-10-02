5d883a46672b142f9cc5ca6fd34b1608c804590d
Gatteras <2396398@gmail.com>
2023-10-02 20:29:00 +0300

    second commit

--- vowels.py
+++ vowels.py
@@ -1,18 +1,18 @@
 
-vowels = ['a', 'e', 'i', 'o', 'u', 'y']
-word = input("Provide a word to search for vowels: ")
-
+# vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+vowels2 = set('aeiouy')
+vowels3 = ('a', 'e', 'i', 'o', 'u', 'y')
 vowels_dict = {}
 found = []
-word_consonant = []
+word = input("Provide a word to search for vowels: ")
 
+found2 = sorted(list(vowels2.intersection(set(word))))
 for letter in word:
-    if letter in vowels:
+    if letter in vowels3:
         vowels_dict.setdefault(letter, 0)
         vowels_dict[letter] += 1
         if letter not in found:
             found.append(letter)
-
-print(sorted(found))
+print(sorted(found), found2)
 for k, v in sorted(vowels_dict.items()):
     print(k, v, sep=':')
