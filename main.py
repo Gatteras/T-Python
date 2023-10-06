0727797138167b54297f1e1e8f8313046750ba2b
Gatteras <2396398@gmail.com>
2023-10-07 00:04:04 +0300

    first commit code

--- main.py
+++ main.py
@@ -6,6 +6,10 @@
 import time
 from datetime import datetime
 import random
+import vsearch
+
+print(vsearch.search_vowels('krokodile'))
+exit()
 
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
         21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
